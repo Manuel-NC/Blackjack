6de7dbcbb68678a63066da281424c6e08d1b26e1
Manuel <manu40.navarro@gmail.com>
2026-08-28 13:32:53 -0700

    Agregado la clase Jugador, falta modificar el controlador para adaptarse a la clase Jugador para empezar a hacer el juego de verdad.

---
+++ src/main/java/com/example/blackjack/Modelo/Jugador.java
@@ -0,0 +1,40 @@
+package com.example.blackjack.Modelo;
+
+public class Jugador {
+    private String nombre;
+    private ManoBlackjack mano;
+    private boolean plantado;
+
+    public Jugador(String nombre) {
+        this.nombre = nombre;
+        this.plantado = false;
+        this.mano = new ManoBlackjack();
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public boolean getPlantado(){
+        return plantado;
+    }
+
+    public ManoBlackjack getMano(){
+        return mano;
+    }
+
+    public void plantarse(){
+        this.plantado = true;
+    }
+
+    public void reiniciarMano(){
+        mano.limpiar();
+        plantado = false;
+    }
+
+    @Override
+    public String toString(){
+        return nombre + ":" + mano.toString();
+    }
+
+}
