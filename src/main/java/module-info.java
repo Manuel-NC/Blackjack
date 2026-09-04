b5f94f1e1b865a9c012de87480cb7447819606e2
Manuel <manu40.navarro@gmail.com>
2026-09-04 05:31:04 -0700

    Dividi la GUI en 2, 1 crea toda la interfaz del tablero y la otra gestiona el juego en si, dandole acciones a los botones con el uso de la Vista y el Controlador.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,12 @@ module com.example.blackjack {
     requires javafx.controls;
     requires javafx.fxml;
 
+    // Abrir y exportar el paquete donde esta la GUI
+    exports com.example.blackjack.Vista;
+    opens com.example.blackjack.Vista to javafx.graphics, javafx.fxml;
 
-    opens com.example.blackjack to javafx.fxml;
+    // Exportar el resto de paquetes
+    exports com.example.blackjack.Controlador;
+    exports com.example.blackjack.Modelo;
     exports com.example.blackjack;
 }
